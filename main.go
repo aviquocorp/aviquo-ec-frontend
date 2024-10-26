3e83220e3e29e96b5fa2ddca07f66e2d4e710a4a
sraaga2020 <63835764+sraaga2020@users.noreply.github.com>
2024-10-25 17:12:19 -0700

    Update main.go

--- main.go
+++ main.go
@@ -12,7 +12,6 @@ func main() {
 	initializeEc()
 	http.HandleFunc("/sat", ServeForm)
 	http.HandleFunc("/sat/find-questions", FindQuestionsHandler)
-	http.HandleFunc("/sat/view-details", ViewQuestionDetailsHandler)
 	fmt.Println("Server is running on http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
 }
